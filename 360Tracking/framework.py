d64e4c7b52c720c0b2c18f26cfb4be9bb773160a
nancywang <yaya3208123@sina.com>
2014-05-14 14:03:18 +0800

    update chinese into English

--- 360Tracking/framework.py
+++ 360Tracking/framework.py
@@ -8,26 +8,25 @@ import operate as op
 # test_date = constant.DATE
 
 def main():
-#创建测试目录
+#Create test dir
     if op.check_dir()  is True:
-        print u"待测试目录 %s 已存在" % op.dir_path
+        print 'Test Dir  %s was exist' % op.dir_path
     else:
-        print u"正在创建待测试目录"
+        print 'Creating Test Dir'
         print "="*80
-        print u"测试目录 %s 创建成功" % op.create_dir()
-# 创建测试文件
+        print 'Create Test Dir %s Succeed' % op.create_dir()
+# Create Test Log File
     if op.check_file()  is True:
-        print u"待测试文件 %s 已存在" % op.file_path
+        print "Test Log File  %s  was exist" % op.file_path
     else:
-        print u"正在创建待测试文件"
+        print "Creating Test Log File"
         print "="*80
-        print u"测试文件 %s 创建成功" % op.create_file()
-#创建测试数据
-    print u"开始创建测试数据"
+        print "'Create Test Log File  %s Succeed" % op.create_file()
+# Create Test Data
+    print "Starting to create test data"
     op.create_data()
     print "="*80
     print "Finished!"
-
 
 #if __name__ == '__main__':
 main()
